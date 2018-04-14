7b741edc8f637bed1d53facc83e834c4d8056d46
Ken Guie <nyhunter77@yahoo.com>
2018-04-14 16:04:46 -0400

    more oauth

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken);
+}));
+
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', { // passport knows to identify the strategy as 'google'
+    scope: ['profile', 'email'] // what specific information in user's google account we want access to
+  })
+);
 
 // app.get('/', (req, res) => {  // no longer needed
 //   res.send({ hi: 'there' });
